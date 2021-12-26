0ceb008745e47a13f6d6876aa0e1f35b413c1494
“李吉业” <lijiye@avicare.cn>
2021-12-26 13:36:09 +0800

    host

--- config/index.js
+++ config/index.js
@@ -1,13 +1,7 @@
 'use strict'
-// Template version: 1.3.1
-// see http://vuejs-templates.github.io/webpack for documentation.
-
 const path = require('path')
-
 module.exports = {
   dev: {
-
-    // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
@@ -22,8 +16,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    // host: '192.168.5.242', // can be overwritten by process.env.HOST
-    host: '192.168.1.35', // can be overwritten by process.env.HOST
+    host: '192.168.5.242', // can be overwritten by process.env.HOST
+    // host: '192.168.1.35', // can be overwritten by process.env.HOST
     port: 8096, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
