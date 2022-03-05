22e64d03bea7d692d1985536b1c1764c019b4677
“李吉业” <lijiye@avicare.cn>
2022-03-05 21:50:25 +0800

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,8 @@ module.exports = {
         user: 'root',                      //ssh 登陆服务器用户名
         host: '121.89.245.28',              //ssh 地址服务器domain/IP
         ref: 'origin/master',             //Git远程/分支
-        repo: 'git@github.com:Bharmik/Misty.git',         //git地址使用ssh地址
+        repo: 'https://github.com/Bharmik/Misty.git',         //git地址使用ssh地址
+        // repo: 'git@github.com:Bharmik/Misty.git',         //git地址使用ssh地址
         ssh_options: 'StrictHostKeyChecking=no',
         path: '/home/mistyy',       //项目存放服务器文件路径
         "post-deploy": 'npm install && pm2 reload ecosystem.config.js --env production'  //部署后的动作
